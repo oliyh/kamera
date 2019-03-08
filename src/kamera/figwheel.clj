27c0a161a693862ad774fd7c7358dcecd6f84a9c
Oliver Hine <git@oliy.co.uk>
2019-03-08 14:05:21 +0000

    Wait even longer for devcards to load

--- src/kamera/figwheel.clj
+++ src/kamera/figwheel.clj
@@ -88,7 +88,7 @@
    (log/info "Navigating to" devcards-url)
    (.navigate session devcards-url)
    (.waitDocumentReady session 15000)
-   (Thread/sleep 2000)
+   (Thread/sleep 10000) ;; wait for devcards to load fully
    (when init-hook
      (init-hook session))
    (let [target-urls (find-test-urls session)]
