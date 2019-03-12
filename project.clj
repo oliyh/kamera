fa43bdc4fa4bcdcd4a8ddf4bf8302288d2065386
Oliver Hine <git@oliy.co.uk>
2019-03-12 22:10:00 +0000

    Revert "Reducing dependencies"
    This reverts commit 7340018697452f76c4dd47857b41e117f05cdece.

--- project.clj
+++ project.clj
@@ -3,14 +3,15 @@
   :url "https://github.com/oliyh/kamera"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[me.raynes/conch "0.8.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/tools.reader "1.3.2"]
+                 [me.raynes/conch "0.8.0"]
                  [doo-chrome-devprotocol "0.1.0"]
+                 [devcards "0.2.6"]
+                 [com.bhauman/figwheel-main "0.1.9"]
                  [hickory "0.7.1"]]
   :resource-paths ["example/resources"]
   :monkeypatch-clojure-test false
-  :profiles {:provided {:dependencies [[org.clojure/clojure "1.10.0"]
-                                       [org.clojure/tools.reader "1.3.2"]
-                                       [com.bhauman/figwheel-main "0.1.9"]]}
-             :dev {:dependencies [;; required for example project
+  :profiles {:dev {:dependencies [;; required for example project
                                   [reagent "0.8.1"]
                                   [devcards "0.2.6"]]}})
