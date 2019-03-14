467f45cf1e37f8d491a7ed47c457114d94ff4061
Oliver Hine <git@oliy.co.uk>
2019-03-14 23:38:23 +0000

    Reducing artifact size

--- project.clj
+++ project.clj
@@ -6,7 +6,6 @@
   :dependencies [[me.raynes/conch "0.8.0"]
                  [doo-chrome-devprotocol "0.1.0"]
                  [hickory "0.7.1"]]
-  :resource-paths ["example/resources"]
   :monkeypatch-clojure-test false
   :profiles {:provided {:dependencies [[org.clojure/clojure "1.10.0"]
                                        [org.clojure/clojurescript "1.10.339"]
@@ -14,4 +13,5 @@
                                        [com.bhauman/figwheel-main "0.1.9"]]}
              :dev {:dependencies [;; required for example project
                                   [reagent "0.8.1"]
-                                  [devcards "0.2.6"]]}})
+                                  [devcards "0.2.6"]]
+                   :resource-paths ["example/resources"]}})
