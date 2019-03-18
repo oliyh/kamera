25f7c5d9b64a4040079ceb497217af62b895444f
Oliver Hine <git@oliy.co.uk>
2019-03-18 22:11:46 +0000

    Fixing example project with ns rename

--- example/test/example/kamera_test.clj
+++ example/test/example/kamera_test.clj
@@ -1,15 +1,15 @@
 (ns example.kamera-test
-  (:require [kamera.figwheel :as kf]
+  (:require [kamera.devcards :as kd]
             [clojure.test :refer [deftest testing is]]))
 
 (deftest devcards-test
   (println "Running example project kamera tests")
   (let [build-id "dev"
-        opts (-> kf/default-opts
+        opts (-> kd/default-opts
                  (update :default-target merge {:reference-directory "test-resources/kamera"
                                                 :screenshot-directory "target/kamera"})
                  ;; enable to turn off headless mode
                  ;; (assoc-in [:chrome-options :chrome-args] [])
                  )]
 
-    (kf/test-devcards build-id opts)))
+    (kd/test-devcards build-id opts)))
