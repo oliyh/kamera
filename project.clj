f77cdde29348a2aed95922d0c15b211ed9d6d232
Oliver Hine <git@oliy.co.uk>
2019-05-01 21:44:36 +0100

    Version 0.1.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kamera "0.1.1"
+(defproject kamera "0.1.2-SNAPSHOT"
   :description "UI testing via image comparison and devcards"
   :url "https://github.com/oliyh/kamera"
   :license {:name "Eclipse Public License"
