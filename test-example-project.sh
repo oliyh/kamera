a46d72f1ea6756596bbaa0fcda359ed9dcebb8f2
Oliver Hine <git@oliy.co.uk>
2020-07-08 14:53:19 +0100

    Better assertions on example project, closes #16

--- test-example-project.sh
+++ test-example-project.sh
@@ -2,6 +2,8 @@
 
 cd example
 
+rm -rf ./target
+
 lein test
 
 if [ $? -eq 0 ]; then
@@ -9,4 +11,11 @@ if [ $? -eq 0 ]; then
     exit 1;
 else
     echo "Example project failed as expected"
+    FILE=./target/kamera/results.edn
+    if [ -f "$FILE" ]; then
+        echo "Report $FILE generated as expected";
+    else
+        echo "Report $FILE was not generated, failing";
+        exit 1;
+    fi
 fi
